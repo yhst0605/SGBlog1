e6ea984f53b5055a861d1427ce7a16f46a1fe2f0
zhouyuhang <zhouyuhang0605@163.com>
2023-05-24 20:09:56 +0800

    initial commit

---
+++ sangeng-blog/src/main/java/com/sangeng/runner/ViewCountRunner.java
@@ -0,0 +1,34 @@
+package com.sangeng.runner;
+
+import com.sangeng.domain.entity.Article;
+import com.sangeng.service.ArticleService;
+import com.sangeng.utils.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+public class ViewCountRunner implements CommandLineRunner {
+
+    @Autowired
+    private ArticleService articleService;
+
+    @Autowired
+    private RedisCache redisCache;
+
+    @Override
+    public void run(String... args) {
+        //查询博客信息  id  viewCount
+        List<Article> articles = articleService.list();
+        Map<String, Integer> viewCountMap = articles.stream()
+                .collect(Collectors.toMap(article -> article.getId().toString(), article -> {
+                    return article.getViewCount().intValue();//
+                }));
+        //存储到redis中
+        redisCache.setCacheMap("article:viewCount",viewCountMap);
+    }
+}
